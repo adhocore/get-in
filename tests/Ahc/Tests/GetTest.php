4537ba0f1a2ebbc6a5154e906d6189d4978fab7d
Jitendra Adhikari <jiten.adhikary@gmail.com>
2014-08-30 11:49:16 +0545

    indent
    tabs to 4 spaces

--- tests/Ahc/Tests/GetTest.php
+++ tests/Ahc/Tests/GetTest.php
@@ -6,60 +6,60 @@ use Ahc\Get as GetIn;
 
 class GetTest extends \PHPUnit_Framework_TestCase
 {
-	public function testWithAllMethodsAndValues()
-	{
-		$this->assertEquals(
-			'permission name', 
-			GetIn::in(
-				new User(new Group(new Permission('permission name'))), 
-    			'getGroup.getPermission.getName'
-    		)
-		);
+    public function testWithAllMethodsAndValues()
+    {
+        $this->assertEquals(
+            'permission name', 
+            GetIn::in(
+                new User(new Group(new Permission('permission name'))), 
+                'getGroup.getPermission.getName'
+            )
+        );
 
-		// methods as array
-		$this->assertEquals(
-			'permission name', 
-			GetIn::in(
-				new User(new Group(new Permission('permission name'))), 
-    			['getGroup', 'getPermission', 'getName']
-    		)
-		);
-	}
+        // methods as array
+        $this->assertEquals(
+            'permission name', 
+            GetIn::in(
+                new User(new Group(new Permission('permission name'))), 
+                ['getGroup', 'getPermission', 'getName']
+            )
+        );
+    }
 
-	public function testWithDefaultPermissionName()
-	{
-		$this->assertEquals(
-			'default permission name', 
-			GetIn::in(
-				new User(new Group(new Permission())), 
-    			'getGroup.getPermission.getName',
-    			'default permission name'
-    		)
-		);
-	}
+    public function testWithDefaultPermissionName()
+    {
+        $this->assertEquals(
+            'default permission name', 
+            GetIn::in(
+                new User(new Group(new Permission())), 
+                'getGroup.getPermission.getName',
+                'default permission name'
+            )
+        );
+    }
 
-	public function testMissingMethod()
-	{
-		$permission = new Permission();
-		$this->assertEquals(
-			$permission, 
-			GetIn::in(
-				new User(new Group($permission)), 
-    			'getGroup.getPermission'
-    		)
-		);		
-	}
+    public function testMissingMethod()
+    {
+        $permission = new Permission();
+        $this->assertEquals(
+            $permission, 
+            GetIn::in(
+                new User(new Group($permission)), 
+                'getGroup.getPermission'
+            )
+        );      
+    }
 
-	public function testMissingMethodWithDefault()
-	{
-		$otherPermission = new Permission('other');
-		$this->assertEquals(
-			$otherPermission,
-			GetIn::in(
-				new User(new Group()), 
-    			'getGroup.getPermission',
-    			$otherPermission
-    		)    		
-		);
-	}
+    public function testMissingMethodWithDefault()
+    {
+        $otherPermission = new Permission('other');
+        $this->assertEquals(
+            $otherPermission,
+            GetIn::in(
+                new User(new Group()), 
+                'getGroup.getPermission',
+                $otherPermission
+            )           
+        );
+    }
 }
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,20 +1,20 @@
 <?php 
 
 if ( ! is_file($vendorLoader = __DIR__.'/../vendor/autoload.php')) {
-	spl_autoload_register(function($class){
-		$class = ltrim($class, '\\');
-		if (0 === stripos($class, 'Ahc')){
-			$path =  dirname(__DIR__) . DIRECTORY_SEPARATOR;
-			$path .= 0 === stripos($class, 'Ahc\\Tests') ? 'tests' : 'lib';
-			$path .= DIRECTORY_SEPARATOR;
+    spl_autoload_register(function($class){
+        $class = ltrim($class, '\\');
+        if (0 === stripos($class, 'Ahc')){
+            $path =  dirname(__DIR__) . DIRECTORY_SEPARATOR;
+            $path .= 0 === stripos($class, 'Ahc\\Tests') ? 'tests' : 'lib';
+            $path .= DIRECTORY_SEPARATOR;
 
-			require_once $path . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
+            require_once $path . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
 
-			return true;
-		}
-	});
+            return true;
+        }
+    });
 
-	return;
+    return;
 }
 
 $loader = require_once $vendorLoader;
